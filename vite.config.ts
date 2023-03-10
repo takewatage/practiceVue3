cf8884217cfaa81129ff8902ce8fccb730d5027e
ishizuka takeaki <>
2023-03-10 10:56:29 +0900

    chore: env設定

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [vue()],
+export default defineConfig(({mode}) => {
+  return {
+    plugins: [vue(), htmlPlugin(loadEnv(mode, "."))],
+  }
 })
+
+function htmlPlugin(env: ReturnType<typeof loadEnv>) {
+  return {
+    name: "html-transform",
+    transformIndexHtml: {
+      enforce: "pre" as const,
+      transform: (html: string): string =>
+          html.replace(/%(.*?)%/g, (match, p1) => env[p1] ?? match),
+    },
+  }
+}
