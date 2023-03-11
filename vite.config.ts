1e907309f97e0a8481f7b12f7c2fbddace5f466a
ishizuka takeaki <>
2023-03-11 16:36:27 +0900

    chore: sass setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,22 @@
 import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import autoprefixer from "autoprefixer";
 
 // https://vitejs.dev/config/
-export default defineConfig(({mode}) => {
-  return {
-    plugins: [vue(), htmlPlugin(loadEnv(mode, "."))],
+export default defineConfig(({command, mode}) => {
+  if (command === 'serve') {
+    return {
+      plugins: [vue(), htmlPlugin(loadEnv(mode, "."))],
+    }
+  } else {
+    return {
+      plugins: [vue(), htmlPlugin(loadEnv(mode, "."))],
+      css: {
+        postcss: {
+          plugins: [autoprefixer],
+        },
+      },
+    }
   }
 })
 
